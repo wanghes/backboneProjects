745da1294ec1fa9050118c3b2f8c06abdec73e2a
wang_hes <543508570@qq.com>
2016-09-17 22:50:32 +0800

    tjiao

--- project/test_server/upAction/UploadAction.php
+++ project/test_server/upAction/UploadAction.php
@@ -5,6 +5,7 @@ function extend($file_name){
     $extend = strtolower($extend["extension"]);
     return $extend;
 }
+$mulu = '';
 if(count($_FILES) > 0)
 {
     try {
@@ -14,28 +15,28 @@ if(count($_FILES) > 0)
     $ext = extend($name);
     $image_name = time().rand(100,999).".".$ext;
     $tmp = $f['tmp_name'];
-    $dateInfo = '/upload/girls/'.$dataCur;
+    $dateInfo = 'upload/girls/'.$dataCur;
     if(!is_dir($dateInfo)){
         mkdir($dateInfo,0777,true);
 
-chmod($dateInfo, 0777);
+        chmod($dateInfo, 0777);
     } 
 
     if(file_exists($dateInfo))  
-      echo "目录创建成功！";  
+      $mulu="目录创建成功！";  
 else  
-      echo "创建目录失败！";  
+      $mulu= "创建目录失败！";  
 
     $filename = $dateInfo .'/'.$image_name;
 
-    move_uploaded_file($tmp,'../'.$filename);
+    move_uploaded_file($tmp,$filename);
     } catch (Exception $e) {   
     print $e->getMessage();   
     exit();   
 }
 
 
-    exit(json_encode(array("status"=>true,"path"=>$filename,"msg"=>"上传成功！")));
+    exit(json_encode(array("status"=>true,"path"=>$filename,"msg"=>"上传成功！",'mulu'=>$mulu)));
 
 }
 else
